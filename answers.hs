a366cf6bacc804adb389665cfa4e327e81547520
Brett Lischalk <brett@brettlischalk.com>
2014-05-15 20:58:30 -0500

    Adding problem 7 def.

--- answers.hs
+++ answers.hs
@@ -56,3 +56,10 @@ myReverse xs = foldl (\acc n -> n : acc) [] xs
 -- True
 isPalindrome [] = False
 isPalindrome xs = xs == reverse xs
+
+-- Problem 7
+-- Flatten a nested list structure.
+-- Transform a list, possibly holding lists as elements into a 'flat' list by
+-- replacing each list with its elements (recursively).
+-- flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+-- [1,2,3,4,5]
